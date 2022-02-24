7b403417fef7168759cedde3105af536f7ddb3d5
alamgirjuab <alamgirjuab@gmail.com>
2022-02-24 21:26:23 +0600

    2nd commit

--- src/component/TodoList/TodoList.tsx
+++ src/component/TodoList/TodoList.tsx
@@ -1,22 +1,34 @@
 import React, { ChangeEvent, FC, useEffect, useState } from 'react';
 import './TodoList.css';
 
+interface iTask {
+    taskName: string,
+    deadline: number,
+}
 const TodoList: FC = () => {
     const [task, setTask] = useState<string>('');
     const [deadline, setDeadline] = useState<number>(0);
-    const [todo, setTodo] = useState([]);
+    const [todoList, setTodoList] = useState<iTask[]>([]);
 
-    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        if (event.target.name === "task") {
+            setTask(event.target.value)
+        } else {
+            setDeadline(Number(event.target.value))
+        }
 
     }
 
+    const addTask = (): void => {
+        setTodoList([...todoList])
+    }
     return (
         <div>
             <h2>Todo List Web App</h2>
             <div className="input-area">
-                <input className="custom-style" type="text" placeholder="Input Your Task Here" onChange={handleChange} /><br />
-                <input className="custom-style" type="number" placeholder="Deadline" onChange={handleChange} /><br />
-                <button className="custom-style">Add Task</button>
+                <input className="custom-style" type="text" name="task" placeholder="Input Your Task Here" onChange={handleChange} /><br />
+                <input className="custom-style" type="number" name="deadline" placeholder="Deadline" onChange={handleChange} /><br />
+                <button className="custom-style" onClick={addTask}>Add Task</button>
             </div>
         </div>
     )
